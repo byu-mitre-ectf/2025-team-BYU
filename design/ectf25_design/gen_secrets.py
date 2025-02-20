f3b7a96e88b8130bd361071d4258596041e4fdea
Macen <macen.bird@gmail.com>
2025-02-19 18:05:07 -0700

    removed some debug statements

--- design/ectf25_design/gen_secrets.py
+++ design/ectf25_design/gen_secrets.py
@@ -92,9 +92,9 @@ def gen_secrets(channels: list[int], args):
     poly_key_array = str(list(poly1305_key))[1:-1]
     chacha_zero_array = str(list(chacha_keys[0]))[1:-1]
 
-    print(f"Poly Key: {poly_key_array}")
-    print(f"Chacha Key: {chacha_zero_array}")
-    print(f"RSA Key: {rsa_private_array}")
+    # print(f"Poly Key: {poly_key_array}")
+    # print(f"Chacha Key: {chacha_zero_array}")
+    # print(f"RSA Key: {rsa_private_array}")
 
     header_file_content = f"""#ifndef SECRETS_H
 #define SECRETS_H
