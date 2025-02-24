c59d9f2ac0c2a744f5aa9be576a374a0ace6030d
Macen <macen.bird@gmail.com>
2025-02-24 16:34:12 -0700

    simple syntax error fixed for the trand disable bit

--- decoder/src/decoder.c
+++ decoder/src/decoder.c
@@ -483,7 +483,7 @@ void init() {
 
     // Enable TRNG : page 91
     volatile uint32_t* pclkdis1 = (volatile uint32_t*)GCR_PCLKDIS1_BASE;
-    *pclkdis1 = ~GCR_TRAND_DISABLE;
+    *pclkdis1 &= ~GCR_TRAND_DISABLE;
 
     // initialize the UART peripheral to enable serial I/O
     ret = uart_init();
